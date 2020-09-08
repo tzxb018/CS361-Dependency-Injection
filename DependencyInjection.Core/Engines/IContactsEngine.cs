d02f3953237417723b084b01b9e3f66746f2479d
Tomo Bessho <tomobessho018@gmail.com>
2020-09-08 12:40:00 -0500

    installed unity

---
+++ DependencyInjection.Core/Engines/IContactsEngine.cs
@@ -0,0 +1,22 @@
+﻿using DependencyInjection.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Core.Engines
+{
+    public interface IContactsEngine
+    {
+        IEnumerable<Contact> GetContacts();
+
+        Contact GetContact(int id);
+
+        Contact InsertContact(Contact newContact);
+
+        Contact UpdateContact(int id, Contact updated);
+
+        Contact DeleteContact(int id);
+    }
+}
